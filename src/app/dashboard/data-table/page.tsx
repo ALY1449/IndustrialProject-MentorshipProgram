73d1e58f1144568c388835ed9e1ebad36abe8720
Alyssa <alyssapausanos1449@gmail.com>
2024-03-25 13:43:59 +1300

    display data to home page table

--- src/app/dashboard/data-table/page.tsx
+++ src/app/dashboard/data-table/page.tsx
@@ -4,59 +4,53 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import Avatar from '@mui/material/Avatar';
 import { Button } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, doc, setDoc, getDoc, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, doc, setDoc, getDoc, getDocs, query, where, onSnapshot } from 'firebase/firestore';
 import { useDispatch } from 'react-redux';
-import { progressData } from '@/app/redux/features/registration/dashboardSlice';
-
-const app = initializeApp({
-  apiKey: "AIzaSyD34-PzQb_WxlQvRrt8a2vH5oUzmr0CzKk",
-  authDomain: "mentorshipapplicationform.firebaseapp.com",
-  projectId: "mentorshipapplicationform",
-  storageBucket: "mentorshipapplicationform.appspot.com",
-  messagingSenderId: "664284335203",
-  appId: "1:664284335203:web:459803b8dc8a1a6574bc56"
-});
+import database from '@/app/firestore/firestore';
 
 const DataTable: React.FC<ChildProps> = (props) => {
   const dispatch = useDispatch();
   const [tabValue, setTabValue] = useState('1');
+  const [rows, setRows] = useState([]);
 
-  const db = getFirestore(app);
-  const docRef = doc(db, "mentor", "Alyssa Pausanos"); //read from alyssa collection in mentor page
+  const q = query(collection(database, "Mentees"), where("documentOf", "!=", null));
 
   useEffect(() => {
     const fetchData = async () => {
-      const docSnap = await getDoc(docRef);
-      try {
-        if (docSnap.exists()) {
-          console.log("Document data:", docSnap.data());
-        } else {
-          // docSnap.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      } catch (error) {
-        console.error("Error fetching document:", error);
-      }
+      const querySnapshot = await getDocs(q);
+      const newRows = [];
+      querySnapshot.forEach((doc, index) => {
+        // Construct new data object
+        const newData = {
+          id: doc.data()['documentOf'] + '_' + index,
+          avatar: 'G',
+          fullName: doc.data()['documentOf'],
+          registeredOn: doc.data()['createdAt'].toDate(),
+          status: doc.data()['status'],
+          assignedMentor: doc.data()['assignedMentor']
+        };
+        newRows.push(newData);
+      });
+      // Update the state with the new rows
+      setRows(newRows);
     };
 
     fetchData(); // Call the async function
-  }, [docRef]);
+  }, [q]);
 
-  const matchMentee = (mentee: string) =>{
+
+  const matchMentee = (mentee: string) => {
     props.allocateMentee(mentee);
     setTabValue('2');
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     props.changeTab(tabValue);
-  },[tabValue]);
+  }, [tabValue]);
 
-
-  useEffect(()=>{
+  useEffect(() => {
     //dispatch(progressData());
-  })
-  
+  });
   const columns: GridColDef[] = [
     { field: 'avatar', headerName: 'Avatar', width: 200, renderCell: (avatarIcon) => <Avatar />},
     { field: 'fullName', headerName: 'Full Name', width: 227 },
@@ -78,14 +72,6 @@ const DataTable: React.FC<ChildProps> = (props) => {
     }},
   ];
 
-  const rows = [
-    { id: 'A', avatar: 'A', fullName: 'Alyssa', registeredOn: '1/01/24', status: 'COMPLETED', assignedMentor: 'Mentor 1'},
-    { id: 'B', avatar: 'H', fullName: 'Ann', registeredOn: '1/01/24', status: 'INCOMPLETE', actions: 'ASSIGN A MENTOR'},
-    { id: 'C', avatar: 'H', fullName: 'Chachot', registeredOn: '1/01/24', status: 'INCOMPLETE', actions: 'ASSIGN A MENTOR'},
-    { id: 'D', avatar: 'B', fullName: 'Ivan', registeredOn: '1/01/24', status: 'COMPLETED', assignedMentor: 'Mentor 3'},
-    { id: 'E', avatar: 'H', fullName: 'Matthew', registeredOn: '1/01/24', status: 'COMPLETED', assignedMentor: 'Mentor 4'},
-    { id: 'F', avatar: 'H', fullName: 'Freddy', registeredOn: '1/01/24', status: 'COMPLETED', assignedMentor: 'Mentor 5'}
-  ];
   return (
     <div style={{ height: 400, width: '100%', maxWidth: '83.2%' }}>
       <DataGrid
@@ -93,10 +79,10 @@ const DataTable: React.FC<ChildProps> = (props) => {
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[10, 15]}
         rowSelection
         sx={{'& .MuiDataGrid-columnHeader': {backgroundColor:"#8F3880"}, 
         '& .MuiDataGrid-columnHeaderTitle  ': {color: 'white'}}}
