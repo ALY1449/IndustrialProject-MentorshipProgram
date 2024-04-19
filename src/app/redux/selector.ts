71855c50a67a2a769eca6969edb97b46a3b8d68d
Alyssa <alyssapausanos1449@gmail.com>
2024-04-19 17:07:55 +1200

    ignore firestore file

--- src/app/redux/selector.ts
+++ src/app/redux/selector.ts
@@ -1,10 +1,16 @@
 import { RootState } from "./store";
 
-export const personalDetailsSelector = (state: RootState) => state.registration.user;
-export const mentorProfessionalDetailsSelector = (state: RootState) => state.registration.mentorProfessionalDetailsData;
-export const mentorPreferencesSelector = (state: RootState) => state.registration.mentorPreferences;
+export const personalDetailsSelector = (state: RootState) =>
+  state.registration.user;
+export const mentorProfessionalDetailsSelector = (state: RootState) =>
+  state.registration.mentorProfessionalDetailsData;
+export const mentorPreferencesSelector = (state: RootState) =>
+  state.registration.mentorPreferences;
 export const skillsSelector = (state: RootState) => state.registration.skills;
 export const goalsSelector = (state: RootState) => state.registration.goals;
-export const personalityTypeSelector = (state: RootState) => state.registration.personalityType;
-export const menteePreferencesSelector = (state:RootState)=> state.registration.menteePreferences;
-export const menteeEducationalBackgroundSelector = (state: RootState)=> state.registration.menteeEducationalBackground;
+export const personalityTypeSelector = (state: RootState) =>
+  state.registration.personalityType;
+export const menteePreferencesSelector = (state: RootState) =>
+  state.registration.menteePreferences;
+export const menteeEducationalBackgroundSelector = (state: RootState) =>
+  state.registration.menteeEducationalBackground;
